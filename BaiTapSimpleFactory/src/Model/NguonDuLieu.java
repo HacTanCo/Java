1714ddc411e5195556515ce499709366da99d7fd
Turquoise <130583274+HacTanCo@users.noreply.github.com>
2025-03-24 16:17:23 +0700

    Delete BaiTapSimpleFactory directory

--- BaiTapSimpleFactory/src/Model/NguonDuLieu.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import java.util.ArrayList;
-
-public interface NguonDuLieu {
-	public ArrayList<SinhVien> DocDanhSachSV() throws Exception;
-	public void ThemSinhVien(SinhVien sv) throws Exception;
-	public void XoaSinhVien(String maSinhVien) throws Exception;
-}
